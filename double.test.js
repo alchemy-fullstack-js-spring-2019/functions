536905a2ec06dba4c49957a691bb3f4c7b1d74eb
Chris Piccaro <chrispiccaro18@gmail.com>
2019-04-01 10:22:59 -0700

    another way to write tests

--- double.test.js
+++ double.test.js
@@ -1,5 +1,12 @@
 const double = require('./double');
 
-test('doubles 2 to 4', () => {
-  expect(double(2)).toBe(4);
+// test('doubles 2 to 4', () => {
+//   expect(double(2)).toBe(4);
+// });
+
+describe('double function', () => {
+  it('doubles the number 2', () => {
+    const result = double(2);
+    expect(result).toEqual(4);
+  });
 });
