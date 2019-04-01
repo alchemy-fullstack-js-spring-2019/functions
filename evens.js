1579696422a0b9bfd5298dec5af34dfe27ca6531
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-01 11:35:56 -0700

    evens now includes firstEven and uses an isEven fn

--- evens.js
+++ evens.js
@@ -1,5 +1,16 @@
-function evens(listOfNumbers) {
-  return listOfNumbers.filter(number => number % 2 === 0);
+function isEven(number) {
+  return number % 2 === 0;
 }
 
-module.exports = evens;
+function evens(listOfNumbers) {
+  return listOfNumbers.filter(isEven);
+}
+
+function firstEven(listOfNumbers) {
+  return listOfNumbers.findIndex(isEven);
+}
+
+module.exports = {
+  evens,
+  firstEven
+};
--- evens.test.js
+++ evens.test.js
@@ -1,4 +1,4 @@
-const evens = require('./evens.js');
+const { evens, firstEven } = require('./evens.js');
 
 describe('evens function', () => {
   it('accepts array of numbers, returns an array with only even numbers', () => {
@@ -6,4 +6,17 @@ describe('evens function', () => {
     const result = evens(testArray);
     expect(result).toEqual([2, 8]); 
   });
+
+  describe('first evens function', () => {
+    it('returns the index of the first even number in an array', () => {
+      const expected = 2;
+  
+      const input = [1, 1, 2, 3, 5, 8, 13];
+  
+      const result = firstEven(input);
+  
+      expect(result).toEqual(expected);
+    });
+  });
 });
+
