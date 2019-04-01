e105cb3c979f81573fa3f16044ca427180f6ecea
Cosmo Kent <cosmolightfoot@gmail.com>
2019-04-01 12:03:21 -0700

    tests pass

---
+++ add.test.js
@@ -0,0 +1,8 @@
+const add = require('./add');
+
+describe('cumlative sum of array entries', () => {
+    it('adds all entries of array', () => {
+        const arrayIn = [0, 1, 2, 3, 5, 8];
+        expect(add(arrayIn)).toEqual(19);
+    });
+});
--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 console.log('hello');
+
+
+function double(number) {
+  return number * 2;
+}
+
+const test = double(16);
+
+console.log(test);
