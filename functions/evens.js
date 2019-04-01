1644f3d67ccc97db69a44cb55d2bb739065cb156
Cheri Portman <cheriportman@hotmail.com>
2019-04-01 14:04:34 -0700

    finished tdd-functions exercises

--- functions/evens.js
+++ functions/evens.js
@@ -1,15 +1,22 @@
-// function evens(array) {
-//   return array.filter((num) => {
-//     (num % 2) === 0);
-// }
+const add = require('./add.js');
 
+const isEven = n => n % 2 === 0;
 
-// const isEven = n => n % 2 === 0;
+const evens = arr => arr.filter(isEven);
 
-// const evens = arr => arr.filter(isEven);
+const firstEven = arr => arr.findIndex(isEven);
 
-function evens(arr) {
-  return arr.filter((num) => num % 2 === 0);
-}
+const allEven = arr => arr.every(isEven);
 
-module.exports = evens;
+// const addEvens = arr => arr.add(evens);
+
+const addEvens = (arr) => {
+  if(allEven(arr) === true) {
+    return add(arr);
+  }
+  else {
+    return 'no dice';
+  }
+};
+
+module.exports = { evens, firstEven, allEven, addEvens };
--- functions/evens.test.js
+++ functions/evens.test.js
@@ -1,4 +1,4 @@
-const evens = require('./evens.js');
+const { evens, firstEven, allEven, addEvens } = require('./evens.js');
 
 describe('return only the even numbers of an array', () => {
   it('given {1, 2, 3, 4], returns [2, 4]', () => {
@@ -7,3 +7,28 @@ describe('return only the even numbers of an array', () => {
     expect(result).toEqual([2, 4]);
   });
 });
+
+describe('take an array and return the first even number', () => {
+  it('given [1, 2, 3, 4] returns 2', () => {
+    expect(firstEven([1, 2, 3])).toEqual(1);
+  });
+});
+
+describe('take an array and return if all are even, false if any are odd', () => {
+  it('given [2, 4, 6], returns true', () => {
+    expect(allEven([2, 4, 6])).toEqual(true);
+  });
+  it('given [1, 2, 3, 4], returns false', () => {
+    expect(allEven([1, 2, 3, 4])).toEqual(false);
+  });
+});
+
+describe('take an array, return sum if the numbers in array are all even', () => {
+  it('given [2, 4, 6}, returns 12', () => {
+    expect(addEvens([2, 4, 6])).toEqual(12);
+  });
+  it('given [1, 2, 3], return no dice', () => {
+    expect(addEvens([1, 2, 3])).toEqual('no dice');
+  });
+});
+
