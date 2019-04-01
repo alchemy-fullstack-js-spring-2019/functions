cdbb0147d093816ee20844adc649ab5d0d4068e0
Payton Lower <paytown@me.com>
2019-04-01 15:06:52 -0700

    working

--- number-manipulator.test.js
+++ number-manipulator.test.js
@@ -2,7 +2,7 @@ const { numberManipulator, repeater } = require('./number-manipulator');
 
 describe('hof', () => {
     it('can manipulate a number', () => {
-        // not too sure on this one :\
+        // not too sure what i'm supposed to be doing on this one.
     });
 
     it('can repeat a fn multiple times', () => {
