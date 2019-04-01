37cf2253aa8a239a76de21e2f3bea2754c12eb7b
ollinieminen <otn987@gmail.com>
2019-04-01 10:43:50 -0700

    create power.js and test it

---
+++ double.test.js
@@ -0,0 +1,9 @@
+const double = require('./double');
+
+
+describe('doubles number', ()=>{
+    it('doubles the number',()=>{
+        expect (double(2)).toBe(4);
+    });
+   
+}); 
---
+++ power.js
@@ -0,0 +1,4 @@
+function power(number, power){
+  return Math.pow(number, power);
+}
+module.exports = power;
