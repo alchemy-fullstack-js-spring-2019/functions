0929c1ae39c537d1082c198c7e86ab1119c8b514
Tommy Tran <tranttommy95@gmail.com>
2019-04-01 12:01:13 -0700

    refactor!!!!

--- evens.js
+++ evens.js
@@ -1,24 +1,29 @@
 const sumArray = require('./add.js');
 
-function isEven(n) {
-  return !(n % 2);
-}
+// function isEven(n) {
+//   return !(n % 2);
+// }
+const isEven = n => !(n % 2);
 
-function evenArray(arr) {
-  return arr.filter(isEven);
-}
+// function evenArray(arr) {
+//   return arr.filter(isEven);
+// }
+const evenArray = arr => arr.filter(isEven);
 
-function locateFirstEven(arr) {
-  return arr.findIndex(isEven);
-}
+// function locateFirstEven(arr) {
+//   return arr.findIndex(isEven);
+// }
+const locateFirstEven = arr => arr.findIndex(isEven);
 
-function isEvenArray(arr) {
-  return arr.every(isEven);
-}
+// function isEvenArray(arr) {
+//   return arr.every(isEven);
+// }
+const isEvenArray = arr => arr.every(isEven);
 
-function sumEvenArray(arr) {
-  return sumArray(evenArray(arr));
-}
+// function sumEvenArray(arr) {
+//   return sumArray(evenArray(arr));
+// }
+const sumEvenArray = arr => sumArray(evenArray(arr));
 
 module.exports = {
   evenArray,
