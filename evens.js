e2d8b7dbcf5b615e882fab1eb6630080a376c102
Ben Parisot <me@benparisot.com>
2019-04-01 11:31:13 -0700

    write addEvens function

--- evens.js
+++ evens.js
@@ -12,7 +12,15 @@ function allEvens(array) {
     return array.every(checkEven);
 }
 
+function addEvens(array) {
+    const evensArray = evens(array);
+    return evensArray.reduce(function(total, num) {
+        return total + num;
+    });
+}
+
 module.exports = {
     evens,
-    allEvens
+    allEvens,
+    addEvens
 };
--- evens.test.js
+++ evens.test.js
@@ -1,4 +1,4 @@
-const { evens, allEvens } = require('./evens');
+const { evens, allEvens, addEvens } = require('./evens');
 
 describe('evens array', () => {
     it('returns only even numbers', () => {
@@ -9,5 +9,11 @@ describe('evens array', () => {
 describe('all evens array', () => {
     it('returns true if all numbers in array are even', () => {
         expect(allEvens([1, 2, 4])).toBe(false);
+    });
+});
+
+describe('add evens array', () => {
+    it('returns sum of all evens in array', () => {
+        expect(addEvens([1, 4, 3, 4])).toBe(8);
     });
 });
