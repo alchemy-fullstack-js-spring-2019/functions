f220930a680a205b924ec04ae2950e1b61aeb6b7
Marty Martinez <manuel.marty.martinez@gmail.com>
2019-04-01 10:53:22 -0700

    adapted double to include doubleArray

--- double.js
+++ double.js
@@ -1,5 +1,14 @@
 function double(n) {
   return n * 2;
 }
+
+function doubleArray(listOfNumbers) {
+  return listOfNumbers.map(number => {
+    return double(number);
+  });
+}
 //export default double
-module.exports = double;
+module.exports = {
+  double,
+  doubleArray
+};
--- double.test.js
+++ double.test.js
@@ -1,10 +1,18 @@
 //import double from ./double.js
-const double = require('./double,js');
+const { double, doubleArray } = require('./double.js');
 
 describe('double function', () => {
   it('doubles the number 2', () => {
     const result = double(2);
     expect(result).toEqual(4);
+  });
+});
+
+describe('doubleArray function', () => {
+  it('doubles each number in an array', () => {
+    const testArray = [1, 1, 2, 3, 5, 8, 13];
+    const result = doubleArray(testArray);
+    expect(result).toEqual([2, 2, 4, 6, 10, 16, 26]);
   });
 });
 
