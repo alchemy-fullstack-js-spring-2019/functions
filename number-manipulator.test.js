f4921efd41d5bd5b7400377cb9be7c2f70e12265
Payton Lower <paytown@me.com>
2019-04-02 09:20:40 -0700

    number manipulator passing

--- number-manipulator.test.js
+++ number-manipulator.test.js
@@ -5,6 +5,10 @@ describe('hof', () => {
         expect(numberManipulator(4, n => n * 2)).toEqual(8);
     });
 
+    it('adds 2 to a number', () => {
+        expect(numberManipulator(10, n => n + 2)).toEqual(12);
+    });
+
     it('can repeat a fn multiple times', () => {
         const mock = jest.fn();
         repeater(10, mock);
