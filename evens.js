7e2f3bbb0e64dd977c7435a614f066ede9b0228c
Phoebe LaBlonde <pmlablonde2220@gmail.com>
2019-04-02 08:52:17 -0700

    all fns passing

--- evens.js
+++ evens.js
@@ -1,5 +1,6 @@
 const add = require('./add');
 
+//without methods
 // function evenOnly(array) {
 //   const evenArray = [];
 //   for(let i = 0; i < array.length; i++) {
@@ -10,21 +11,43 @@ const add = require('./add');
 //   return evenArray
 // }
 
+//easy way
 function evenOnly(array) {
   return array.filter(item => item % 2 === 0)
 }
+//--------------------------------------------------------------------------
 
+//without recommended method
 function firstEven(array) {
   const firstEvenNum = evenOnly(array)[0];
   return array.indexOf(firstEvenNum);
 }
-//could also use findIndex (easier way??)
-
 //easy way
+//could also use findIndex
+
+//--------------------------------------------------------------------------
+
 function checkIfEven(item) {
   return item % 2 === 0;
 
 }
+//--------------------------------------------------------------------------
+
+//without methods 
+// function ifAllEven(array) {
+//   let total = 0;
+//   for(let i = 0; i < array.length; i++) {
+//     if(array[i] % 2 === 0) {
+//       total+=1;
+//     }
+//   }
+//   if(total === array.length) {
+//     return 'true';
+//   }
+//   return 'false';
+// }
+
+//easy way
 function ifAllEven(array) {
   if(array.every(checkIfEven)) {
     return 'true';
@@ -32,6 +55,11 @@ function ifAllEven(array) {
   return 'false'
 }
 
+//--------------------------------------------------------------------------
+//without methods
+
+
+//easy way
 function addEvens(array) {
   const evenArray = evenOnly(array);
   return add(evenArray);
--- evens.test.js
+++ evens.test.js
@@ -15,12 +15,12 @@ describe('tests arrays to find even numbers', () => {
   it('takes an array and returns true if all are even', () => {
     const fakeArray = [2, 4, 6];
     const expected = 'true';
-    expect(ifAllEven(fakeArray)).toEqual(expected);
+    expect(ifAllEven(fakeArray)).toEqual(expected); //same as calling toBeTruthy  ()
   });
   it('takes an array and returns false if all are not even', () => {
     const fakeArray = [1, 3, 6];
     const expected = 'false';
-    expect(ifAllEven(fakeArray)).toEqual(expected);
+    expect(ifAllEven(fakeArray)).toEqual(expected); //same as calling toBeFalsey  ()
   });
   it('takes an array, creates a new array with only evens, then returns the sum of even numbers', () => {
     const fakeArray = [5, 6, 7, 8, 9, 10];
