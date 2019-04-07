77dc67d3b640c3d7eb0f120afbe2e1a4a78ad86f
Masmics <cherlocker3113@gmail.com>
2019-04-07 11:19:40 -0700

    added Higher Order Functions, all Jest tests passing

--- hof.js
+++ hof.js
@@ -2,6 +2,22 @@ function numberManipulator(n, callback) {
   return callback(n);
 }
 
+function repeater(n, callback) {
+  for(let i = 0; i < n; i++) {
+    callback();
+  }
+}
+
+function map(array, callback) {
+  let arr2 = [];
+  for(let i = 0; i < array.length; i++) {
+    let temp = callback(array[i]);
+    arr2.push(temp);
+  }
+}
+
 module.exports = {
   numberManipulator,
+  repeater,
+  map
 };
